4c8fc68a4cda237d272477985423d010164b7f73
Tommaso Tocci <tommaso@tocci.pro>
2018-09-18 08:54:42 +0200

    intercept getdents

--- ifs/include/preload/adafs_functions.hpp
+++ ifs/include/preload/adafs_functions.hpp
@@ -62,7 +62,9 @@ ssize_t adafs_pread_ws(int fd, void* buf, size_t count, off64_t offset);
 
 int adafs_opendir(const std::string& path);
 
-struct dirent * adafs_readdir(int fd);
+int getdents(unsigned int fd,
+             struct linux_dirent *dirp,
+             unsigned int count);
 
 int adafs_rmdir(const std::string& path);
 
